68071a85ea29c30090a07f850cfd181ca9c4fbe8
Jean-Paul <jean.paul.bassil@outlook.com>
2025-02-22 14:18:53 +0200

    chore: Remove console log from home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,7 +3,6 @@ import { useAuth } from '@/components/providers/AuthProvider'
 
 export default function Home() {
   const { user } = useAuth()
-  console.log('user', user)
   return (
     <div>
       <h1>{user?.email || 'No user'}</h1>
